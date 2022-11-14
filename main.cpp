ab7a5a5ec7bfb6358766e5f3bcff358e96734ea2
jacopomazzo <114420544+jacopomazzo@users.noreply.github.com>
2022-11-14 17:28:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,17 @@ using namespace std;
 
 int main() {
    float a=0, b=0, x=0, y=0, z=0;
-    cout<<"Inserire i coefficenti a e b: ";
-    cin>>a>>b;
-    cout<<"Inserire le incognite x e y: ";
-    cin>>x>>y;
-
-    if ((x<0) && (y>0)){
-        z = a*x - b*y;
-        cout<<"Risultato equazione = "<<z<<endl;
-    } else if ((x>=0) && (y<=0)){
-        z = a*(x*x) - b*y;
-        cout<<"Risultato equazione = "<<z<<endl;
-    } else{
-        z = a*x + b*(y*y);
-        cout<<"Risultato equazione = "<<z<<endl;
-
-    }
+   cin>>a>>b;
+   cin>>x>>y;
+   if ((x<0) && (y>0)){
+      z = a*x - b*y;
+      cout<<z<<endl;
+   } else if ((x>=0) && (y<=0)){
+      z = a*(x*x) - b*y;
+      cout<<z<<endl;
+   } else{
+      z = a*x + b*(y*y);
+      cout<<"z<<endl;
+      }
    return 0;
 }
